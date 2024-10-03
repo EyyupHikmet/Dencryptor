f28ab6b1fbfe15cb13d38fbff945cbc5e0b4d246
ekunduraci <EKunduraci@ahlatciteknoloji.com.tr>
2024-10-03 17:55:33 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+	alias(libs.plugins.android.application)
+	alias(libs.plugins.kotlin.android)
+	alias(libs.plugins.ksp)
+	alias(libs.plugins.compose.compiler)
+	alias(libs.plugins.hilt.android)
+}
+
+android {
+	namespace = "com.rdiykru.dencryptor"
+	compileSdk = 34
+
+	defaultConfig {
+		applicationId = "com.rdiykru.dencryptor"
+		minSdk = 28
+		targetSdk = 34
+		versionCode = 1
+		versionName = "1.0"
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		vectorDrawables {
+			useSupportLibrary = true
+		}
+	}
+
+	buildTypes {
+		release {
+			isShrinkResources = true
+			isMinifyEnabled = true
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+	kotlinOptions {
+		jvmTarget = "17"
+	}
+	buildFeatures {
+		compose = true
+		buildConfig = true
+	}
+	packaging {
+		resources {
+			excludes += "/META-INF/{AL2.0,LGPL2.1}"
+			// Exclude the file from one of the dependencies
+			excludes += "/META-INF/gradle/incremental.annotation.processors"
+		}
+	}
+}
+
+dependencies {
+
+	implementation(libs.androidx.core.ktx)
+	implementation(libs.androidx.activity.compose)
+	implementation(platform(libs.androidx.compose.bom))
+	implementation(libs.androidx.ui)
+	implementation(libs.androidx.ui.graphics)
+	implementation(libs.androidx.ui.tooling.preview)
+	implementation(libs.androidx.material3)
+
+	// Hilt with KSP
+	ksp(libs.hilt.compiler)
+	implementation(libs.hilt.android)
+	implementation(libs.hilt.android.compiler)
+	implementation(libs.androidx.hilt.navigation.compose)
+
+	// DataStore
+	implementation(libs.androidx.datastore.core)
+	implementation(libs.androidx.datastore.preferences)
+	implementation(libs.androidx.lifecycle.livedata.ktx)
+
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(libs.androidx.espresso.core)
+	androidTestImplementation(platform(libs.androidx.compose.bom))
+	androidTestImplementation(libs.androidx.ui.test.junit4)
+	debugImplementation(libs.androidx.ui.tooling)
+	debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ app/src/main/java/com/rdiykru/DencryptorApp.kt
@@ -0,0 +1,8 @@
+package com.rdiykru
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DencryptorApp: Application() {
+}
